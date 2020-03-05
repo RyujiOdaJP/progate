14f1e3367b9b94f42295bba0b681e41e10c573da
RyujiOdaJP <ryuji.oda@gmail.com>
2020-03-05 11:52:17 +0900

    unziped SQL files

---
+++ SQL/study4/page3/exercise1.sql
@@ -0,0 +1,7 @@
+-- studentsテーブルからidカラムの値が7のレコードを削除してください。
+DELETE FROM students
+
+WHERE id = 7
+;
+-- 下記のクエリは消さないでください。
+SELECT * FROM students;
---
+++ SQL/study4/page4/page1/exercise1.sql
@@ -0,0 +1,6 @@
+-- studentsテーブルにデータを追加してくださ
+INSERT INTO students (name, course)
+VALUES('Kate', 'Java')
+;
+-- 下記のクエリは消さないでください。
+select * from students;
